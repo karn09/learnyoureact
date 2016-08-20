cad461ce7c27c0370290b7d0649de8814790da1c
John Nieves <karn09@gmail.com>
2016-08-20 19:22:11 -0400

    move inline styles to style var

--- views/index.jsx
+++ views/index.jsx
@@ -17,9 +17,7 @@ class TodoList extends React.Component {
 	render() {
 		return (
 			<div className="todoList">
-				<table style={{
-					border: "2px solid black"
-				}}>
+				<table style={style.table}>
 					<tbody>
 						<Todo title="Shopping">Milk</Todo>
 						<Todo title="Hair cut">13:00</Todo>
@@ -58,8 +56,8 @@ class Todo extends React.Component {
 				<td style={{border: "1px solid black"}}>
 					<input type="checkbox" checked={this.state.checked} onChange={this.handleChange.bind(this)}></input>
 				</td>
-				<td style={{border: "1px solid black"}}>{this.props.title}</td>
-				<td style={{border: "1px solid black"}}>{this.props.children}</td>
+				<td style={style.tableContent}>{this.props.title}</td>
+				<td style={style.tableContent}>{this.props.children}</td>
 			</tr>
 		)
 	}
@@ -76,5 +74,14 @@ class TodoForm extends React.Component {
 				I am a TodoForm.
 			</div>
 		)
+	}
+}
+
+let style = {
+	tableContent: {
+		border: "1px solid black"
+	},
+	table: {
+		border:"2px solid black"
 	}
 }
